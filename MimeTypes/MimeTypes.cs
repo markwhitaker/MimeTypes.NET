5448b41b5eabc89510196a6cf01220750ceeb59c
Mark Whitaker <mark@mainwave.co.uk>
2019-10-20 22:54:00 +0100

    Added comments to consts, for better autocomplete tips

--- MimeTypes/MimeTypes.cs
+++ MimeTypes/MimeTypes.cs
@@ -10,35 +10,154 @@
         /// </summary>
         public static class Application
         {
+            /// <summary>
+            /// application/atom+xml
+            /// </summary>
             public const string AtomXml = "application/atom+xml";
+
+            /// <summary>
+            /// application/atomcat+xml
+            /// </summary>
             public const string AtomcatXml = "application/atomcat+xml";
+            
+            /// <summary>
+            /// application/ecmascript
+            /// </summary>
             public const string Ecmascript = "application/ecmascript";
+            
+            /// <summary>
+            /// application/java-archive
+            /// </summary>
             public const string JavaArchive = "application/java-archive";
+            
+            /// <summary>
+            /// application/javascript
+            /// </summary>
             public const string Javascript = "application/javascript";
+            
+            /// <summary>
+            /// application/json
+            /// </summary>
             public const string Json = "application/json";
+            
+            /// <summary>
+            /// application/mp4
+            /// </summary>
             public const string Mp4 = "application/mp4";
+            
+            /// <summary>
+            /// application/octet-stream
+            /// </summary>
             public const string OctetStream = "application/octet-stream";
+            
+            /// <summary>
+            /// application/pdf
+            /// </summary>
             public const string Pdf = "application/pdf";
+            
+            /// <summary>
+            /// application/pkcs10
+            /// </summary>
             public const string Pkcs10 = "application/pkcs10";
+            
+            /// <summary>
+            /// application/pkcs7-mime
+            /// </summary>
             public const string Pkcs7Mime = "application/pkcs7-mime";
+            
+            /// <summary>
+            /// application/pkcs7-signature
+            /// </summary>
             public const string Pkcs7Signature = "application/pkcs7-signature";
+            
+            /// <summary>
+            /// application/pkcs8
+            /// </summary>
             public const string Pkcs8 = "application/pkcs8";
+            
+            /// <summary>
+            /// application/postscript
+            /// </summary>
             public const string Postscript = "application/postscript";
+            
+            /// <summary>
+            /// application/rdf+xml
+            /// </summary>
             public const string RdfXml = "application/rdf+xml";
+            
+            /// <summary>
+            /// application/rss+xml
+            /// </summary>
             public const string RssXml = "application/rss+xml";
+            
+            /// <summary>
+            /// application/rtf
+            /// </summary>
             public const string Rtf = "application/rtf";
+            
+            /// <summary>
+            /// application/smil+xml
+            /// </summary>
             public const string SmilXml = "application/smil+xml";
+
+            /// <summary>
+            /// application/x-font-otf
+            /// </summary>
             public const string XFontOtf = "application/x-font-otf";
+            
+            /// <summary>
+            /// application/x-font-ttf
+            /// </summary>
             public const string XFontTtf = "application/x-font-ttf";
+            
+            /// <summary>
+            /// application/x-font-woff
+            /// </summary>
             public const string XFontWoff = "application/x-font-woff";
+            
+            /// <summary>
+            /// application/x-pkcs12
+            /// </summary>
             public const string XPkcs12 = "application/x-pkcs12";
+            
+            /// <summary>
+            /// application/x-shockwave-flash
+            /// </summary>
             public const string XShockwaveFlash = "application/x-shockwave-flash";
+            
+            /// <summary>
+            /// application/x-silverlight-app
+            /// </summary>
             public const string XSilverlightApp = "application/x-silverlight-app";
+            
+            /// <summary>
+            /// application/xhtml+xml
+            /// </summary>
             public const string XhtmlXml = "application/xhtml+xml";
+            
+            /// <summary>
+            /// application/xml
+            /// </summary>
             public const string Xml = "application/xml";
+            
+            /// <summary>
+            /// application/xml-dtd
+            /// </summary>
             public const string XmlDtd = "application/xml-dtd";
+            
+            /// <summary>
+            /// application/xslt+xml
+            /// </summary>
             public const string XsltXml = "application/xslt+xml";
+            
+            /// <summary>
+            /// application/x-www-form-urlencoded
+            /// </summary>
             public const string XWwwFormUrlencoded = "application/x-www-form-urlencoded";
+            
+            /// <summary>
+            /// application/zip
+            /// </summary>
             public const string Zip = "application/zip";
         }
 
@@ -47,16 +166,59 @@
         /// </summary>
         public static class Audio
         {
+            /// <summary>
+            /// audio/midi
+            /// </summary>
             public const string Midi = "audio/midi";
+            
+            /// <summary>
+            /// audio/mp4
+            /// </summary>
             public const string Mp4 = "audio/mp4";
+            
+            /// <summary>
+            /// audio/mpeg
+            /// </summary>
             public const string Mpeg = "audio/mpeg";
+            
+            /// <summary>
+            /// audio/ogg
+            /// </summary>
             public const string Ogg = "audio/ogg";
+            
+            /// <summary>
+            /// audio/wav
+            /// </summary>
             public const string Wav = "audio/wav";
+            
+            /// <summary>
+            /// audio/webm
+            /// </summary>
             public const string Webm = "audio/webm";
+            
+            /// <summary>
+            /// audio/x-aac
+            /// </summary>
             public const string XAac = "audio/x-aac";
+            
+            /// <summary>
+            /// audio/x-aiff
+            /// </summary>
             public const string XAiff = "audio/x-aiff";
+            
+            /// <summary>
+            /// audio/x-mpegurl
+            /// </summary>
             public const string XMpegurl = "audio/x-mpegurl";
+            
+            /// <summary>
+            /// audio/x-ms-wma
+            /// </summary>
             public const string XMsWma = "audio/x-ms-wma";
+            
+            /// <summary>
+            /// audio/x-wav
+            /// </summary>
             public const string XWav = "audio/x-wav";
         }
 
@@ -65,12 +227,39 @@
         /// </summary>
         public static class Image
         {
+            /// <summary>
+            /// image/bmp
+            /// </summary>
             public const string Bmp = "image/bmp";
+            
+            /// <summary>
+            /// image/gif
+            /// </summary>
             public const string Gif = "image/gif";
+            
+            /// <summary>
+            /// image/jpeg
+            /// </summary>
             public const string Jpeg = "image/jpeg";
+            
+            /// <summary>
+            /// image/png
+            /// </summary>
             public const string Png = "image/png";
+            
+            /// <summary>
+            /// image/svg+xml
+            /// </summary>
             public const string SvgXml = "image/svg+xml";
+            
+            /// <summary>
+            /// image/tiff
+            /// </summary>
             public const string Tiff = "image/tiff";
+            
+            /// <summary>
+            /// image/webp
+            /// </summary>
             public const string Webp = "image/webp";
         }
 
@@ -79,9 +268,24 @@
         /// </summary>
         public static class Multipart
         {
+            /// <summary>
+            /// multipart/byteranges
+            /// </summary>
             public const string Byteranges = "multipart/byteranges";
+            
+            /// <summary>
+            /// multipart/encrypted
+            /// </summary>
             public const string Encrypted = "multipart/encrypted";
+            
+            /// <summary>
+            /// multipart/form-data
+            /// </summary>
             public const string FormData = "multipart/form-data";
+            
+            /// <summary>
+            /// multipart/related
+            /// </summary>
             public const string Related = "multipart/related";
         }
 
@@ -90,12 +294,39 @@
         /// </summary>
         public static class Text
         {
+            /// <summary>
+            /// text/css
+            /// </summary>
             public const string Css = "text/css";
+            
+            /// <summary>
+            /// text/csv
+            /// </summary>
             public const string Csv = "text/csv";
+            
+            /// <summary>
+            /// text/html
+            /// </summary>
             public const string Html = "text/html";
+            
+            /// <summary>
+            /// text/plain
+            /// </summary>
             public const string Plain = "text/plain";
+            
+            /// <summary>
+            /// text/richtext
+            /// </summary>
             public const string RichText = "text/richtext";
+            
+            /// <summary>
+            /// text/sgml
+            /// </summary>
             public const string Sgml = "text/sgml";
+            
+            /// <summary>
+            /// text/yaml
+            /// </summary>
             public const string Yaml = "text/yaml";
         }
 
@@ -104,12 +335,39 @@
         /// </summary>
         public static class Video
         {
+            /// <summary>
+            /// video/3gpp
+            /// </summary>
             public const string Threegpp = "video/3gpp";
+            
+            /// <summary>
+            /// video/h264
+            /// </summary>
             public const string H264 = "video/h264";
+            
+            /// <summary>
+            /// video/mp4
+            /// </summary>
             public const string Mp4 = "video/mp4";
+            
+            /// <summary>
+            /// video/mpeg
+            /// </summary>
             public const string Mpeg = "video/mpeg";
+            
+            /// <summary>
+            /// video/ogg
+            /// </summary>
             public const string Ogg = "video/ogg";
+            
+            /// <summary>
+            /// video/quicktime
+            /// </summary>
             public const string Quicktime = "video/quicktime";
+            
+            /// <summary>
+            /// video/webm
+            /// </summary>
             public const string Webm = "video/webm";
         }
     }
