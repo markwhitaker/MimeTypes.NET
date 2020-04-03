f45473e41effab17c2d5b3ed088ce749e6036f1c
Mark Whitaker <mark@mainwave.co.uk>
2020-04-04 00:12:33 +0100

    Added font/* mime types

---
+++ MimeTypes.Tests/FontTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace Mainwave.MimeTypes.Tests
+{
+    [TestFixture]
+    public class FontTests
+    {
+        [TestCase(MimeType.Font.Collection, "font/collection")]
+        [TestCase(MimeType.Font.Otf, "font/otf")]
+        [TestCase(MimeType.Font.Sfnt, "font/sfnt")]
+        [TestCase(MimeType.Font.Ttf, "font/ttf")]
+        [TestCase(MimeType.Font.Woff, "font/woff")]
+        [TestCase(MimeType.Font.Woff2, "font/woff2")]
+        public void AssertFontMimeTypeValue(string valueUnderTest, string expectedValue)
+        {
+            Assert.That(valueUnderTest, Is.EqualTo(expectedValue));
+        }
+    }
+}
---
+++ MimeTypes/MimeType.Font.cs
@@ -0,0 +1,43 @@
+﻿namespace Mainwave.MimeTypes
+{
+    public static partial class MimeType
+    {
+        /// <summary>
+        /// MIME type constants for font types
+        /// </summary>
+        public static class Font
+        {
+            private const string Prefix = "font/";
+            
+            /// <summary>
+            /// font/collection
+            /// </summary>
+            public const string Collection = Prefix + "collection";
+            
+            /// <summary>
+            /// font/otf
+            /// </summary>
+            public const string Otf = Prefix + "otf";
+            
+            /// <summary>
+            /// font/sfnt
+            /// </summary>
+            public const string Sfnt = Prefix + "sfnt";
+            
+            /// <summary>
+            /// font/ttf
+            /// </summary>
+            public const string Ttf = Prefix + "ttf";
+            
+            /// <summary>
+            /// font/woff
+            /// </summary>
+            public const string Woff = Prefix + "woff";
+            
+            /// <summary>
+            /// font/woff2
+            /// </summary>
+            public const string Woff2 = Prefix + "woff2";
+        }
+    }
+}
